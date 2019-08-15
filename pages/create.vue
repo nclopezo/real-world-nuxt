9d53e754491ce475fc9df3f887c7c8230bcbac10
David Mendez <dmendez@ebi.ac.uk>
2019-08-15 16:42:14 +0100

    Lesson 4 finished

---
+++ pages/create.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an Event',
+    }
+  }
+}
+</script>
